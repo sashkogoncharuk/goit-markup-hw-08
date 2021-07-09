20eb8445404e9d16b7009d67e6868874c2b055b4
Oleksandr Honcharuk <sashko.goncharuk@gmail.com>
2021-07-09 23:21:36 +0300

    update

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,12 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.mobile--open'),
-    closeMenuBtn: document.querySelector('.mobile--close'),
-    menu: document.querySelector('.menu'),
+    openMenuBtn: document.querySelector('.mobile-open'),
+    closeMenuBtn: document.querySelector('.mobile-close'),
+    menu: document.querySelector('.mobile'),
   };
-
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
   function toggleMenu() {
-    refs.menu.classList.toggle('closed');
+    refs.menu.classList.toggle('is-hidden');
   }
 })();
